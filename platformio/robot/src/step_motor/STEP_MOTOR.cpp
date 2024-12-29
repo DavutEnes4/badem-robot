ce9f4f0c778eecf004bbf2f16e321a238ff28ed8
Your Name <you@example.com>
2024-12-29 10:00:00 +0300

    .

--- platformio/robot/src/step_motor/STEP_MOTOR.cpp
+++ platformio/robot/src/step_motor/STEP_MOTOR.cpp
@@ -1,33 +1,16 @@
 #include <Arduino.h>
 
-/**
- * @brief step_motor Sınıfı
- *
- *  Bu sınıf motor kontrollerini sağlar.
- */
-
 class step_motor
 {
 private:
-    int enable_pin;              // Enable pin
-    int dir_pin;                 // Direction pin
-    int pwm_pin;                 // PWM pin
-    int step_period;             // Step period
-    int period;                  // Period
-    unsigned long previous_time; // Previous time
-    bool forward;                // Forward
-public:
-    /**
-     * @brief Yeni bir step_motor nesnesi oluşturur
-     *
-     * Bu fonksiyon, step_motor nesnesinin başlatılması için gerekli parametreleri alır ve motorun çalışabilmesi için gerekli pinleri ve ayarları yapılandırır.
-     *
-     * @param enable_pin Motorun aktif olup olmayacağını kontrol eden pin.
-     * @param dir_pin Motorun yönünü belirleyen pin.
-     * @param pwm_pin Motorun hızını ayarlayan PWM pin'i.
-     * @param step_period Adım periyodu (her adım arasındaki süreyi belirler).
-     * @param period Motorun çalışma periyodu (gerekli motor frekansını belirler).
-     */
+    int enable_pin;              
+    int dir_pin;                 
+    int pwm_pin;                 
+    int step_period;             
+    int period;                   
+    long previous_time; 
+    bool forward;
+
     step_motor(int enable_pin, int dir_pin, int pwm_pin, int step_period, int period)
     {
         this->enable_pin = enable_pin;
@@ -38,11 +21,6 @@ public:
         this->previous_time = 0;
         this->forward = true;
     }
-    /**
-     * @brief Motoru çalıştırır
-     *
-     * Bu fonksiyon, motorun çalışmasını sağlar.
-     */
     void move()
     {
         unsigned long current_time = micros();
@@ -55,45 +33,19 @@ public:
             this->previous_time = current_time;
         }
     }
-    /**
-     * @brief Motorun durmasını sağlar
-     *
-     * Bu fonksiyon, motorun durmasını sağlar.
-     */
     void stop()
     {
         digitalWrite(this->pwm_pin, LOW);
         digitalWrite(this->enable_pin, HIGH);
     }
-    /**
-     * @brief Motorun çalışıp çalışmadığını kontrol eder
-     *
-     * Bu fonksiyon, motorun çalışıp çalışmadığını kontrol eder.
-     *
-     * @return Motor çalışıyorsa true, çalışmıyorsa false.
-     */
     bool is_moving()
     {
         return digitalRead(this->enable_pin) == LOW;
     }
-    /**
-     * @brief Motorun hızını ayarlar
-     *
-     * Bu fonksiyon, motorun hızını ayarlar.
-     *
-     * @param period Motorun çalışma periyodu (gerekli motor frekansını belirler).
-     */
     void set_speed(int new_period)
     {
         this->period = new_period;
     }
-    /**
-     * @brief Motorun ileri veya geri gitmesini sağlar
-     *
-     * Bu fonksiyon, motorun ileri veya geri gitmesini sağlar.
-     *
-     * @param direction Motorun ileri gitmesi durumunda true, geri gitmesi durumunda false.
-     */
     void set_direction(bool direction)
     {
         this->forward = direction;
